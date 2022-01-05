62b31dcfca84093e33a545621cdd5bb9b338e45f
TKShadowBlade <katanablack85@gmail.com>
2022-01-05 00:38:11 -0500

    setting up ApolloServer and schemas

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
+// importing ApolloServer here:
+const { ApolloServer } = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// accessing ApolloServer here:
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,5 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
