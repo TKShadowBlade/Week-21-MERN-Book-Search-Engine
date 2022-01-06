f580ff739c15d38451de9ed65fcbf78aee5ec6c6
TKShadowBlade <katanablack85@gmail.com>
2022-01-06 16:55:01 -0500

    small resolvers refactoring

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,7 +47,7 @@ const resolvers = {
             if (context.user) {
                 const updatedUser = await user.findOneAndUpdate(
                     {_id: context.user._id},
-                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { $pull: { SavedBook: { bookId: bookId } } },
                     { new: true }
                 )
                 return updatedUser;
