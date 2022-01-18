b5dcc79c39c6c0de67456550864618ee9f2b2a4a
TKShadowBlade <katanablack85@gmail.com>
2022-01-18 00:01:29 -0500

    queries.js cleanup

---
+++ client/build/precache-manifest.9a0c0c1bbaf91bd8bff7d2a8b1adbee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e7f9fb2bf30b5549e9e5209fa1f043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41cb0157bd276da00ab",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "1f012bcfd0620b27e0d4",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "b41cb0157bd276da00ab",
+    "url": "/static/js/2.85301ddb.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.85301ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f012bcfd0620b27e0d4",
+    "url": "/static/js/main.b58e0b41.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const GET_ME = gql`
-  {
+{
     me {
         _id
         username
@@ -14,7 +14,7 @@ export const GET_ME = gql`
                 bookId
                 image
                 link                
-        }
+                }
     }
 }
 `;
